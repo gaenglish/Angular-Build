405d8fc80c8d94f546e874f01160c846523242d4
George <gaenglish@gmail.com>
2016-07-22 00:10:09 -0400

    template file updates

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # Angular-Build
 A starter template for building angular apps with gulp.
+
+
+## Summary
+This template gives you just about everything you need to get started building web-sites with Angular, Angular Material, and Sass.
+
+The template is an extenstion of the [material-start](https://github.com/angular/material-start) project.  A gulp build process has been added to combine and minify javascript files.  [Sass](http://sass-lang.com/) has also been incorporated for generating CSS.
+
+[Font Awesome](http://fontawesome.io/) is added into the build as a bonus.
+
+## Installation
+Installation is easy.  Run ```npm install``` to download all the dependencies needed to run and build your site.
+
+```
+$ npm install
+```
+
+
+## Build
+After running the build, everything you need to deploy to your web server will be located in the ```dist``` folder.
+
+```
+$ gulp build
+```
+
+## Developement
+This template comes with it's own server for running the site locally for developement.  As you save your work, the browser will refresh to reflect your changes.
+
+```
+$ gulp watch
+```
---
+++ gulpfile.js
@@ -0,0 +1,157 @@
+var clean = require('del'),
+    gulp = require('gulp'),
+    uglify = require('gulp-uglify'),
+    sourcemaps = require('gulp-sourcemaps'),
+    concat = require('gulp-concat'),
+    rename = require('gulp-rename'),
+    sass = require('gulp-sass'),
+    minifyCss = require('gulp-clean-css'),
+    runSequence = require('run-sequence'),
+    watch = require('gulp-watch'),
+    shell = require('gulp-shell');
+
+
+var dist = {
+    root: './dist/',
+    js: './dist/assets/js/',
+    css: './dist/assets/css/',
+    images: './dist/assets/images/',
+    svg: './dist/assets/svg/',
+    fonts: './dist/assets/fonts/'
+};
+
+var files = {
+    html: [
+        './src/html/**/*.htm'
+    ],
+    js: [
+        './src/angular/**/*.js'
+    ],
+    css: [
+        './node_modules/angular-material/angular-material.min.css',
+        './node_modules/font-awesome/css/*'
+    ],
+    libs: [
+        './node_modules/html5shiv/dist/html5shiv.min.js',
+        './node_modules/respond-minmax/dest/respond.min.js',
+        './node_modules/angular/angular.min.js',
+        './node_modules/angular-route/angular-route.min.js',
+        './node_modules/angular-aria/angular-aria.min.js',
+        './node_modules/angular-resource/angular-resource.min.js',
+        './node_modules/angular-animate/angular-animate.min.js',
+        './node_modules/angular-material/angular-material.min.js',
+        './node_modules/angular-messages/angular-messages.min.js'
+    ],
+    fonts: [
+        './node_modules/font-awesome/fonts/*.eot',
+        './node_modules/font-awesome/fonts/*.svg',
+        './node_modules/font-awesome/fonts/*.ttf',
+        './node_modules/font-awesome/fonts/*.woff'
+    ],
+    images: [
+        './src/assets/images/**/*'
+    ],
+    svg: [
+        './src/assets/svg/**/*'
+    ]
+};
+
+// Build a locally testable version
+gulp.task('build', function() {
+    runSequence(
+        'clean',
+        'minify',
+        'sass',
+        'copy'
+    );
+});
+
+// Build an unminified version for debugging
+gulp.task('build:debug', function() {
+    runSequence(
+        'clean',
+        'concat',
+        'sass',
+        'copy'
+    );
+});
+
+// Clean the dist directory
+gulp.task('clean', function() {
+    clean.sync([
+        'dist/**/*'
+    ]);
+});
+
+// Build minified JS and sourcemaps
+gulp.task('minify', function() {
+    gulp.src(files.js)
+        .pipe(sourcemaps.init())
+        .pipe(concat('app.min.js'))
+        .pipe(uglify())
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest(dist.js));
+
+    gulp.src(files.js)
+        .pipe(concat('app.js'))
+        .pipe(gulp.dest(dist.js));
+});
+
+// Build a debuggable, concatinated only version of the JS
+gulp.task('concat', function() {
+    gulp.src(files.js)
+        .pipe(concat('app.min.js'))
+        .pipe(gulp.dest(dist.js));
+
+    gulp.src(files.js)
+        .pipe(concat('app.js'))
+        .pipe(gulp.dest(dist.js));
+});
+
+// Compile SASS to CSS
+gulp.task('sass', function() {
+    gulp.src('./src/sass/app.sass')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(minifyCss())
+        .pipe(rename(function(path) {
+            path.extname = '.min.css';
+        }))
+        .pipe(gulp.dest(dist.css));
+});
+
+// Copy static files for local builds to the dist directory
+gulp.task('copy', function() {
+    gulp.src(files.html)
+        .pipe(gulp.dest(dist.root));
+
+    gulp.src(files.libs)
+        .pipe(gulp.dest(dist.js));
+
+    gulp.src(files.images)
+        .pipe(gulp.dest(dist.images));
+
+    gulp.src(files.css)
+        .pipe(gulp.dest(dist.css));
+
+    gulp.src(files.fonts)
+        .pipe(gulp.dest(dist.fonts));
+
+    gulp.src(files.svg)
+        .pipe(gulp.dest(dist.svg));
+
+});
+
+// Launch live server
+gulp.task('serve', shell.task([
+    'cd dist;live-server --open=index.htm --quiet'
+]));
+
+// Watch for changes in the src directory and build the dev version
+gulp.task('watch', function() {
+    gulp.start('serve');
+    gulp.start('build');
+    watch('./src/**/*', function() {
+        gulp.start('build');
+    });
+});
